a8f7d075284313b8c8dcdcfd8ae7ffdf3464bfbd
Lomank <lomank20022@gmail.com>
2021-12-11 18:11:27 +0500

    Update forgot.js
    Small fix

--- frontend/react/assets/js/components/auth/forgot.js
+++ frontend/react/assets/js/components/auth/forgot.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { blankAxiosInstance } from '../../axios';

273b1ebe724467a4a30ca838f26f427743702089
Lomank <lomank20022@gmail.com>
2022-03-27 09:44:34 +0500

    Heroku preparations

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-npm run prod
-npm prune --production
 python manage.py wait_for_db
-python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 

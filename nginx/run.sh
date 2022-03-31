c7db5abc5e5931f0f57aec9a9bbda55b6a8b9d3c
Lomank <lomank20022@gmail.com>
2022-03-31 11:05:23 +0500

    Configured nginx proxy for heroku
    - Also fixed frontend heroku setup

--- nginx/run.sh
+++ nginx/run.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf
+envsubst '\$PORT \$HEROKU_APP_CLIENT_URL \$HEROKU_APP_BACKEND_URL' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'

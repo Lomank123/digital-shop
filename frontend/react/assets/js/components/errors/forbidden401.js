93c1ef8b0a9cb33bb00a9194c9ea16c9e39396c8
Lomank <lomank20022@gmail.com>
2022-04-07 11:35:03 +0500

    Covered /errors folder with translations

--- frontend/react/assets/js/components/errors/forbidden401.js
+++ frontend/react/assets/js/components/errors/forbidden401.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 
 export default function Forbidden() {
-
+  const {t, i18n} = useTranslation();
   return(
     <>
-      <h1>401</h1>
-      <h1>Forbidden</h1>
+      <h1>{t("forbidden-401.number")}</h1>
+      <h1>{t("forbidden-401.label")}</h1>
     </>
   )
 }
--- frontend/react/assets/js/components/errors/notFound404.js
+++ frontend/react/assets/js/components/errors/notFound404.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 
 export default function NotFound() {
-
+  const {t, i18n} = useTranslation();
   return(
     <>
-      <h1>404</h1>
-      <h1>Not found</h1>
+      <h1>{t("not-found-404.number")}</h1>
+      <h1>{t("not-found-404.label")}</h1>
     </>
   )
 }
